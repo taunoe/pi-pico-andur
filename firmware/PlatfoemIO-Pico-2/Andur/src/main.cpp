d5235d40becfbc371347f6bbd3fd81f868de1b6f
Tauno Erik <sedumacre@gmail.com>
2025-12-12 16:19:06 +0200

    uus library

--- firmware/PlatfoemIO-Pico-2/Andur/src/main.cpp
+++ firmware/PlatfoemIO-Pico-2/Andur/src/main.cpp
@@ -4,7 +4,7 @@
  *  -Pico 2
  *  -TOF400C/VL53L1X
  * Started: 29.09.2025
- * Edited:  11.12.2025
+ * Edited:  12.12.2025
  * 
  * Links:
  * - https://arduino-pico.readthedocs.io/en/latest/platformio.html
@@ -23,37 +23,47 @@
  */
 #include <Arduino.h>
 #include <Wire.h>
-//#include "SparkFun_VL53L1X.h"
-#include "Adafruit_VL53L0X.h"  // https://github.com/adafruit/Adafruit_VL53L0X/tree/master
 #include <SPI.h>
+/* Sensor */
+//#include "SparkFun_VL53L1X.h"  // Uuemale sensorile
+#include "Adafruit_VL53L0X.h"  // https://github.com/adafruit/Adafruit_VL53L0X/tree/master
+/* Ekraan */
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
-
+/* Rotary Encoder */
 // https://www.upesy.com/blogs/tutorials/rotary-encoder-raspberry-pi-pico-with-arduino-code#
 #include "hardware/pio.h"
 #include "quadrature.pio.h"  // https://github.com/jamon/pi-pico-pio-quadrature-encoder/tree/main
 
 
-#define LONG  0
-#define SHORT 1
 
-/* Pins */
-// Optional interrupt and shutdown pins for sensor.
-// #define SHUTDOWN_PIN 2 // ?
-// #define INTERRUPT_PIN 3 // ?
-#define STATUS_GREEN_LED   14  //
-#define STATUS_RED_LED     15  //
-#define SELECTED_GREEN_LED 16  // Selected distance
-#define SELECTED_RED_LED   17  // Selected distance
+/********************************
+  Pins
+*********************************/
+// SDA - GPIO4
+// SCL - GPIO5
+#define SENSOR_GPIO1_PIN    6  // Used by the sensor to indicate that data is ready
+#define SENSOR_XSHUT_PIN    7  // By default it's pulled high. When the pin is pulled low, the sensor goes into shutdown mode.
 
-#define MIN_RE_CLK_PIN     18
-#define MIN_RE_DT_PIN      19
-#define MAX_RE_CLK_PIN     20
-#define MAX_RE_DT_PIN      21
+#define STATUS_GREEN_LED    3
+#define STATUS_RED_LED      2
+//#define SELECTED_GREEN_LED 16  // Selected distance
+//#define SELECTED_RED_LED   17  // Selected distance
+
+#define MIN_RE_CLK_PIN     16
+#define MIN_RE_DT_PIN      17
+#define MIN_RE_SW_PIN      18
+#define MAX_RE_CLK_PIN     19
+#define MAX_RE_DT_PIN      20
+#define MAX_RE_SW_PIN      21
+
+#define SIGNAL_OUT_1_PIN   22
+#define SIGNAL_OUT_2_PIN   15
 
 
-
-/* OLED Screen */
+/********************************
+  OLED Screen
+*********************************/
 #define SCREEN_WIDTH      128  // OLED display width, in pixels
 #define SCREEN_HEIGHT      32  // OLED display height, in pixels
 #define OLED_RESET         -1  // Reset pin # (or -1 if sharing Arduino reset pin)
@@ -62,40 +72,43 @@
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 
-/* Laser sensor */
-// adafruit
-Adafruit_VL53L0X lox = Adafruit_VL53L0X();
-/*
-// Sparcfun code
-SFEVL53L1X distance_sensor;
-//Uncomment the following line to use the optional shutdown and interrupt pins.
-//SFEVL53L1X distance_sensor(Wire, SHUTDOWN_PIN, INTERRUPT_PIN);
-//Store distance readings to get rolling average
+/********************************
+  Laser sensor
+  can handle about 30mm to 1200mm of range distance
+*********************************/
 #define DISTANCE_HISTORY_SIZE 3
 int distance_history[DISTANCE_HISTORY_SIZE] = {0};
 uint8_t dis_history_i = 0;
-*/
+
+Adafruit_VL53L0X vl53lxx_sensor = Adafruit_VL53L0X();
 
 
-/* Global Variables */
+/********************************
+  Global Variables
+*********************************/
 long avg_distance = 0;
 uint distance = 0;
 uint selected_value_min = 10;
 uint selected_value_max = 100;
 
 
-
 #define ENCODER_MIN_VALUE         0
 #define ENCODER_MAX_VALUE      4000
 #define ENCODER_SMALLEST_RANGE    5
-/* Rotary Encoder MAX */
+
+/********************************
+  Rotary Encoder MAX
+*********************************/
 int32_t current_encoder_value_max = selected_value_max;
 uint32_t last_pio_count_max = 0;
 PIO re_pio_max = pio0;
 uint re_offset_max;
 uint re_sm_max;
 
-/* Rotary Encoder MIN */
+
+/********************************
+  Rotary Encoder MIN
+*********************************/
 int32_t current_encoder_value_min = selected_value_min;
 uint32_t last_pio_count_min = 0;
 PIO re_pio_min = pio0;
@@ -103,30 +116,32 @@ uint re_offset_min;
 uint re_sm_min;
 
 
-/* Functions */
+/********************************
+  Functions
+*********************************/
 void i2c_scanner();
 void init_LED_pins();
 void init_display();
-void init_sensor();
-void set_sensor_mode(int mode);
+void init_vl53lxx_sensor();
 
+
+/********************************
+  SETUP
+*********************************/
 void setup() {
   Serial.begin(115200); // 921600
-
-  delay(3000);
-  i2c_scanner();
-
   // Do not change! Serial pordi lugemisega tekivad probleemid!!!
   //Wire.setSDA(2); // default 4
   //Wire.setSCL(3); // default 5
   Wire.begin();
-  Wire.setClock(400000); // use 400 kHz I2C
+  //Wire.setClock(400000); // use 400 kHz I2C
+
+  delay(3000);
+  i2c_scanner();
 
   init_LED_pins();
   init_display();
-  init_sensor();
-  set_sensor_mode(LONG);
-
+  init_vl53lxx_sensor();
 
   // Rotary encoder 1 MAX
   re_offset_max = pio_add_program(re_pio_max, &quadratureA_program);
@@ -139,38 +154,41 @@ void setup() {
   quadratureA_program_init(re_pio_min, re_sm_min, re_offset_min, MIN_RE_CLK_PIN, MIN_RE_DT_PIN);
 }
 
-// Core 0
+/********************************
+  Core 0 loop
+*********************************/
 void loop() {
+  // Time
   long start_time = millis();
+  // Sensor
+  VL53L0X_RangingMeasurementData_t measure;
 
-  /* Sensor */
-  distance_sensor.startRanging();  // Write configuration bytes to initiate measurement
+  vl53lxx_sensor.rangingTest(&measure, true); // pass in 'true' to get debug data printout!
 
-  while (!distance_sensor.checkForDataReady()) {
-    delay(1);
-  }
-  distance = distance_sensor.getDistance();  // Get the result of the measurement from the sensor
-  distance_sensor.clearInterrupt();
-  distance_sensor.stopRanging();
-  long end_time = millis();
+  Serial.print("measure.RangeStatus ");
+  Serial.print(measure.RangeStatus);
+  Serial.print(" D=");
+ 
 
-  //Serial.print(distance);  // Kaugus mm
+  if (measure.RangeStatus != 4) {
+    distance = measure.RangeMilliMeter;
+    Serial.println(distance);
 
-  distance_history[dis_history_i] = distance;
+    distance_history[dis_history_i] = distance;
 
-  if (++dis_history_i == DISTANCE_HISTORY_SIZE) {
-    dis_history_i = 0;
-  }
+    if (++dis_history_i == DISTANCE_HISTORY_SIZE) {
+      dis_history_i = 0;
+    }
 
-  for (int x = 0; x < DISTANCE_HISTORY_SIZE; x++) {
-    avg_distance += distance_history[x];
-  }
+    for (int x = 0; x < DISTANCE_HISTORY_SIZE; x++) {
+      avg_distance += distance_history[x];
+    }
 
-  avg_distance /= DISTANCE_HISTORY_SIZE;
-  //Serial.print("\tKeskmine: ");
-  //Serial.print(avg_distance);
+    avg_distance /= DISTANCE_HISTORY_SIZE;
+    //Serial.print("\tKeskmine: ");
+    //Serial.print(avg_distance);
 
-  //////
+     //////
   display.clearDisplay();
 
   display.setTextSize(2);
@@ -194,39 +212,15 @@ void loop() {
   display.display();
   ///
 
-  int signalRate = distance_sensor.getSignalRate();
-  Serial.print("\tSignal rate: ");
-  Serial.print(signalRate);
-
-  byte range_status = distance_sensor.getRangeStatus();
-  Serial.print("\tRange Status: ");
-
-  //Make it human readable
-  switch (range_status)
-  {
-  case 0:
-    Serial.print("Good");
-    break;
-  case 1:
-    Serial.print("Sigma fail");
-    break;
-  case 2:
-    Serial.print("Signal fail");
-    break;
-  case 7:
-    Serial.print("Wrapped target fail");
-    break;
-  default:
-    Serial.print("Unknown: ");
-    Serial.print(range_status);
-    break;
   }
 
-  Serial.print("\tHz: ");
-  Serial.print(1000.0 / (float)(end_time - start_time), 2);
+  delay(100);
 
-  Serial.println();
+  long end_time = millis();
 
+  
+
+ 
 
   /* Rotary Encoders MAX */
   pio_sm_exec_wait_blocking(re_pio_max, re_sm_max, pio_encode_in(pio_x, 32));
@@ -286,36 +280,38 @@ void loop() {
 }
 
 
-// Core 1
+/********************************
+  Core 1 loop
+*********************************/
 void loop1() {
   if (distance < selected_value_min || distance > selected_value_max) {
     // Red LED ON
-    digitalWrite(SELECTED_GREEN_LED, HIGH);
-    digitalWrite(SELECTED_RED_LED, LOW);
+    digitalWrite(STATUS_GREEN_LED, HIGH);
+    digitalWrite(STATUS_RED_LED, LOW);
   }
   else {
     // Green LED ON
-    digitalWrite(SELECTED_GREEN_LED, LOW);
-    digitalWrite(SELECTED_RED_LED, HIGH);
+    digitalWrite(STATUS_GREEN_LED, LOW);
+    digitalWrite(STATUS_RED_LED, HIGH);
   }
 
 }
 
 
-/*
+/********************************
  * Scanns connected I2C devices
  * and prints addresses
- */
+ ********************************/
 void i2c_scanner() {
   byte error, address;
   int nDevices = 0;
-  Serial.println("Scanning...");
+  Serial.println("Scanning I2C ...");
 
   for(address = 1; address < 127; address++ ) {
     Wire.beginTransmission(address);
-    delay(10);
+    //delay(1);
     error = Wire.endTransmission();
-    delay(10);
+    //delay(1);
     if (error == 0) {
       Serial.print("I2C device found at address 0x");
       if (address<16) {
@@ -341,18 +337,18 @@ void i2c_scanner() {
   delay(5000);
 }
 
-/*
-*/
+/********************************
+*********************************/
 void init_LED_pins() {
-  pinMode(SELECTED_GREEN_LED, OUTPUT);
-  pinMode(SELECTED_RED_LED, OUTPUT);
+  pinMode(STATUS_GREEN_LED, OUTPUT);
+  pinMode(STATUS_RED_LED, OUTPUT);
 
-  digitalWrite(SELECTED_RED_LED, LOW);
-  digitalWrite(SELECTED_GREEN_LED, LOW);
+  digitalWrite(STATUS_RED_LED, LOW);
+  digitalWrite(STATUS_GREEN_LED, LOW);
 }
 
-/*
-*/
+/********************************
+*********************************/
 void init_display() {
    // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
    // SSD1306_EXTERNALVCC
@@ -360,7 +356,7 @@ void init_display() {
     Serial.println(F("SSD1306 allocation failed"));
     while (1) {
       Serial.println("Err 1");
-      delay(100);
+      delay(500);
     }
   }
   display.display();
@@ -382,40 +378,20 @@ void init_display() {
   display.display();
 }
 
-/*
-*/
-void init_sensor() {
-  /*
-  if (distance_sensor.begin() != 0) {
-    Serial.println("Sensor connection failed.");
+/********************************
+ * Init TOF sensor
+ * VL53Lxx-v2
+ ********************************/
+void init_vl53lxx_sensor() {
+  Serial.println("Init VL53L0X sensor");
+
+  if (!vl53lxx_sensor.begin()) {
+    Serial.println(F("VL53L0X sensor failed!"));
+
     while (1) {
       Serial.println("Err 2");
       delay(500);
     }
   }
-    */
-    Serial.println("Adafruit VL53L0X test");
-    if (!lox.begin()) {
-      Serial.println(F("Failed to boot VL53L0X"));
-      while(1);
-    }
-/*
-  while (distance_sensor.begin() != 0) {
-    Serial.println("Err 2");
-    Serial.println("Sensor connection failed.");
-    delay(500);
-  }
-  Serial.println("Sensor works!");
-  */
 }
 
-/*
-*/
-void set_sensor_mode(int mode) {
-  if (mode == LONG) {
-    distance_sensor.setDistanceModeLong();
-  }
-  else if (mode == SHORT) {
-    distance_sensor.setDistanceModeShort();
-  }
-}
