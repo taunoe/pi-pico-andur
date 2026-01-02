709da22a16ebad8973e602d851b63e452910a2b1
Tauno Erik <sedumacre@gmail.com>
2026-01-02 16:12:51 +0200

    min,max and btns

--- firmware/PlatfoemIO-Pico-2/Andur/src/main.cpp
+++ firmware/PlatfoemIO-Pico-2/Andur/src/main.cpp
@@ -4,64 +4,64 @@
  *  -Pico 2
  *  -TOF400C/VL53L1X
  * Started: 29.09.2025
- * Edited:  16.12.2025
- * 
+ * Edited:  02.01.2026
+ *
  * Links:
  * - https://arduino-pico.readthedocs.io/en/latest/platformio.html
  * - https://randomnerdtutorials.com/raspberry-pi-pico-i2c-scanner-arduino/
  * - https://github.com/pololu/vl53l1x-arduino/
  * - https://github.com/sparkfun/SparkFun_VL53L1X_Arduino_Library
  * - https://github.com/adafruit/Adafruit_VL53L0X/tree/master
- * 
+ * - https://learn.adafruit.com/adafruit-vl53l0x-micro-lidar-distance-sensor-breakout/arduino-code
+ * - https://github.com/gbr1/rp2040-encoder-library
+ *
  * I2C0: SDA GPIOs -> GPIO0, GPIO4, GPIO8, GPIO12, GPIO16, GPIL20
  * I2C0: SCL GPIOs -> GPIO1, GPIO5, GPIO9, GPIO13, GPIO17, GPIO21
- * 
+ *
  * I2C1: SDA GPIOs -> GPIO2, GPIO6, GPIO10, GPIO14, GPIO18, GPIO26
  * I2C1: SCL GPIOs -> GPIO3, GPIO7, GPIO11, GPIO15, GPIO19, GPIO27
- * 
- * TODO: Mode Short and Long
- * 
+ *
  */
+
 #include <Arduino.h>
 #include <Wire.h>
 #include <SPI.h>
-/* Sensor */
-//#include "SparkFun_VL53L1X.h"  // Uuemale sensorile
-#include "Adafruit_VL53L0X.h"  // https://github.com/adafruit/Adafruit_VL53L0X/tree/master
-/* Ekraan */
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
-/* Rotary Encoder */
-// https://www.upesy.com/blogs/tutorials/rotary-encoder-raspberry-pi-pico-with-arduino-code#
-//#include "hardware/pio.h"
-//#include "quadrature.pio.h"  // https://github.com/jamon/pi-pico-pio-quadrature-encoder/tree/main
-
-#include "pio_encoder.h" // https://github.com/gbr1/rp2040-encoder-library
+#include "Adafruit_VL53L0X.h"  // Sensor
+#include <Adafruit_GFX.h>      // OLED
+#include <Adafruit_SSD1306.h>  // OLED
+#include "pio_encoder.h"       // https://github.com/gbr1/rp2040-encoder-library
+#include <EEPROM.h>
 
 
 /********************************
   Pins
 *********************************/
+// I2C
 // SDA - GPIO4
 // SCL - GPIO5
 #define SENSOR_GPIO1_PIN    6  // Used by the sensor to indicate that data is ready
 #define SENSOR_XSHUT_PIN    7  // By default it's pulled high. When the pin is pulled low, the sensor goes into shutdown mode.
 
-#define STATUS_GREEN_LED    3
-#define STATUS_RED_LED      2
-//#define SELECTED_GREEN_LED 16  // Selected distance
-//#define SELECTED_RED_LED   17  // Selected distance
+#define MIN_VAL             0
+#define MAX_VAL             1
+#define GREEN               2
+#define RED                 3
+#define GREEN_LED_PIN   GREEN
+#define RED_LED_PIN       RED
 
-// Left
+// MIN (Left) Rotary Encoder
 #define MIN_RE_CLK_PIN     16
 #define MIN_RE_DT_PIN      17
 #define MIN_RE_SW_PIN      18
+#define ADDRESS_MIN         0
 
-// Right
+// MAX (Right) Rotary Encoder
 #define MAX_RE_CLK_PIN     19
 #define MAX_RE_DT_PIN      20
 #define MAX_RE_SW_PIN      21
+#define ADDRESS_MAX         4 // A 32-bit integer occupies 4 bytes of memory.
 
+// Signal Out
 #define SIGNAL_OUT_1_PIN   22
 #define SIGNAL_OUT_2_PIN   15
 
@@ -69,10 +69,10 @@
 /********************************
   OLED Screen
 *********************************/
-#define SCREEN_WIDTH      128  // OLED display width, in pixels
-#define SCREEN_HEIGHT      32  // OLED display height, in pixels
+#define SCREEN_WIDTH      128  // px
+#define SCREEN_HEIGHT      32  // px
 #define OLED_RESET         -1  // Reset pin # (or -1 if sharing Arduino reset pin)
-#define SCREEN_ADDRESS   0x3C  // < See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+#define SCREEN_ADDRESS   0x3C  // 0x3D for 128x64, 0x3C for 128x32
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
@@ -93,49 +93,44 @@ Adafruit_VL53L0X vl53lxx_sensor = Adafruit_VL53L0X();
 *********************************/
 long avg_distance = 0;
 uint distance = 0;
-uint selected_value_min = 10;
-uint selected_value_max = 100;
-
+int selected_value_min = 10;
+int selected_value_max = 100;
 
 #define ENCODER_MIN_VALUE         0
-#define ENCODER_MAX_VALUE      4000
+#define ENCODER_MAX_VALUE      1000 //3000
 #define ENCODER_SMALLEST_RANGE    5
 
+
 /********************************
-  Rotary Encoder MAX
+  Rotary Encoders
 *********************************/
-PioEncoder left_rotary_encoder(MIN_RE_CLK_PIN);
-PioEncoder right_rotary_encoder(MAX_RE_CLK_PIN);
-
-/*
-int32_t current_encoder_value_max = selected_value_max;
-uint32_t last_pio_count_max = 0;
-PIO re_pio_max = pio0;
-uint re_offset_max;
-uint re_sm_max;
-*/
+// MIN Rotary Encoder
+PioEncoder min_rotary_encoder(MIN_RE_CLK_PIN);
+// MAX Rotary Encoder
+PioEncoder max_rotary_encoder(MAX_RE_CLK_PIN);
 
 
 /********************************
-  Rotary Encoder MIN
-*********************************/
-/*
-int32_t current_encoder_value_min = selected_value_min;
-uint32_t last_pio_count_min = 0;
-PIO re_pio_min = pio0;
-uint re_offset_min;
-uint re_sm_min;
-*/
-
-
-/********************************
-  Functions
+  Functions prototypes
 *********************************/
 void i2c_scanner();
 void init_LED_pins();
 void init_display();
 void init_vl53lxx_sensor();
 
+void vl53lxx_sensor_OFF();
+void vl53lxx_sensor_ON();
+void init_ecoders();
+void read_buttons();
+void read_encoders();
+void set_status_LED(int color);
+
+uint32_t read_memory(int address);
+int write_memory(int address, uint32_t value);
+uint32_t read_memory_min_value();
+uint32_t read_memory_max_value();
+int write_memory_min_value(uint32_t value);
+int write_memory_max_value(uint32_t value);
 
 /********************************
   SETUP
@@ -155,22 +150,11 @@ void setup() {
   init_display();
   init_vl53lxx_sensor();
 
-  left_rotary_encoder.begin();
-  left_rotary_encoder.flip();
-  right_rotary_encoder.begin();
-  right_rotary_encoder.flip();
+  // Rotary Encoders
+  init_ecoders();
 
-  /*
-  // Rotary encoder 1 MAX
-  re_offset_max = pio_add_program(re_pio_max, &quadratureA_program);
-  re_sm_max = pio_claim_unused_sm(re_pio_max, true);
-  quadratureA_program_init(re_pio_max, re_sm_max, re_offset_max, MAX_RE_CLK_PIN, MAX_RE_DT_PIN);
-
-  // Rotary encoder 2 MIN
-  re_offset_min = pio_add_program(re_pio_min, &quadratureA_program);
-  re_sm_min = pio_claim_unused_sm(re_pio_min, true);
-  quadratureA_program_init(re_pio_min, re_sm_min, re_offset_min, MIN_RE_CLK_PIN, MIN_RE_DT_PIN);
-  */
+  // Initialize EEPROM (allocate 512 bytes of flash)
+  EEPROM.begin(512);
 }
 
 /********************************
@@ -179,20 +163,21 @@ void setup() {
 void loop() {
   // Time
   long start_time = millis();
+
   // Sensor
   VL53L0X_RangingMeasurementData_t measure;
-
   vl53lxx_sensor.rangingTest(&measure, false); // pass in 'true' to get debug data printout!
 
-  Serial.print("measure.RangeStatus ");
-  Serial.print(measure.RangeStatus);
-  Serial.print(" D=");
- 
+  //Serial.print("measure.RangeStatus ");
+  //Serial.print(measure.RangeStatus);
+  //Serial.print(" D=");
 
   if (measure.RangeStatus != 4) {
+    // Distance
     distance = measure.RangeMilliMeter;
-    Serial.println(distance);
+    //Serial.print(distance);
 
+    // Calculate average distamce
     distance_history[dis_history_i] = distance;
 
     if (++dis_history_i == DISTANCE_HISTORY_SIZE) {
@@ -204,16 +189,17 @@ void loop() {
     }
 
     avg_distance /= DISTANCE_HISTORY_SIZE;
-    //Serial.print("\tKeskmine: ");
+    //Serial.print("\tavg=");
     //Serial.print(avg_distance);
+    //Serial.print(" ");
 
-    //////
+    // Display on screen
     display.clearDisplay();
 
     display.setTextSize(2);
     display.setTextColor(SSD1306_WHITE);
     display.setCursor(10,0);
-    display.print(distance);  // avg_distance
+    display.print(distance);  //or avg_distance
     display.println(F(" mm"));
 
     display.setTextSize(1);
@@ -222,25 +208,21 @@ void loop() {
     display.print(F("min:"));
     display.print(selected_value_min);
 
-    //display.setTextSize(1);
-    //display.setTextColor(SSD1306_WHITE);
     display.setCursor(50,20);
     display.print(F("  max:"));
     display.print(selected_value_max);
 
-    display.display();
-  ///
-
+    display.display();  // Update display
   }
-  
+
   long end_time = millis();
 
-  Serial.print("left_rotary_encoder: ");
-  Serial.println(left_rotary_encoder.getCount());
-  Serial.print("right_rotary_encoder: ");
-  Serial.println(right_rotary_encoder.getCount());
+  //vl53lxx_sensor_OFF();
+  //delay(10);
+  //vl53lxx_sensor_ON();
 
-  delay(100);
+  //Serial.println();
+  delay(90);
 }
 
 
@@ -248,83 +230,23 @@ void loop() {
   Core 1 loop
 *********************************/
 void loop1() {
-  /*
-  // Rotary Encoders MAX 
-  pio_sm_exec_wait_blocking(re_pio_max, re_sm_max, pio_encode_in(pio_x, 32));
-  uint32_t new_pio_count_max = pio_sm_get_blocking(re_pio_max, re_sm_max);
+  read_encoders();
+  read_buttons();
 
-  // Calculate the change (delta) since the last reading.
-  // Casting to int32_t correctly handles the wrap-around when the
-  // PIO counter goes from 0 to 4294967295 and vice-versa.
-  int32_t delta_max = (int32_t)new_pio_count_max - (int32_t)last_pio_count_max;
-  last_pio_count_max = new_pio_count_max;
+  //Serial.print("min=");
+  //Serial.print(selected_value_min);
+  //Serial.print("\tmax=");
+  //Serial.print(selected_value_max);
+  //Serial.print("\n");
 
-  current_encoder_value_max += delta_max;
-
-  // Clamp the value to stay within your defined min/max range
-  if (current_encoder_value_max < ENCODER_MIN_VALUE) {
-    current_encoder_value_max = ENCODER_MIN_VALUE;
-  } else if (current_encoder_value_max > ENCODER_MAX_VALUE) {
-    current_encoder_value_max = ENCODER_MAX_VALUE;
-  }
-  selected_value_max = current_encoder_value_max;
-
-  if (selected_value_max <= selected_value_min + ENCODER_SMALLEST_RANGE)
-  {
-    selected_value_max = selected_value_min + ENCODER_SMALLEST_RANGE;
-  }
-
-  printf("MAX Value: %ld\n", current_encoder_value_max);
-
-
-  // MIN 
-  pio_sm_exec_wait_blocking(re_pio_min, re_sm_min, pio_encode_in(pio_x, 32));
-  uint32_t new_pio_count_min = pio_sm_get_blocking(re_pio_min, re_sm_min);
-
-  // Calculate the change (delta) since the last reading.
-  // Casting to int32_t correctly handles the wrap-around when the
-  // PIO counter goes from 0 to 4294967295 and vice-versa.
-  int32_t delta_min = (int32_t)new_pio_count_min - (int32_t)last_pio_count_min;
-  last_pio_count_min = new_pio_count_min;
-
-  current_encoder_value_min += delta_min;
-
-  // Clamp the value to stay within your defined min/max range
-  if (current_encoder_value_min < ENCODER_MIN_VALUE) {
-    current_encoder_value_min = ENCODER_MIN_VALUE;
-  } else if (current_encoder_value_min > ENCODER_MAX_VALUE) {
-    current_encoder_value_min = ENCODER_MAX_VALUE;
-  }
-
-  selected_value_min = current_encoder_value_min;
-
-  if (selected_value_min >= selected_value_max - ENCODER_SMALLEST_RANGE)
-  {
-    selected_value_min = selected_value_max - ENCODER_SMALLEST_RANGE;
-  }
-  
-  printf("MIN Value: %ld\n", current_encoder_value_min);
-  */
-
-  //////////////////////////////////////////
-  if (distance < selected_value_min || distance > selected_value_max) {
-    // Red LED ON
-    digitalWrite(STATUS_GREEN_LED, HIGH);
-    digitalWrite(STATUS_RED_LED, LOW);
-  }
-  else {
-    // Green LED ON
-    digitalWrite(STATUS_GREEN_LED, LOW);
-    digitalWrite(STATUS_RED_LED, HIGH);
-  }
-
+  set_status_LED(RED); // RED or GREEN
 }
 
 
-/********************************
+/******************************************************************
  * Scanns connected I2C devices
- * and prints addresses
- ********************************/
+ * and prints addresses.
+ ******************************************************************/
 void i2c_scanner() {
   byte error, address;
   int nDevices = 0;
@@ -360,18 +282,22 @@ void i2c_scanner() {
   delay(5000);
 }
 
-/********************************
-*********************************/
-void init_LED_pins() {
-  pinMode(STATUS_GREEN_LED, OUTPUT);
-  pinMode(STATUS_RED_LED, OUTPUT);
 
-  digitalWrite(STATUS_RED_LED, LOW);
-  digitalWrite(STATUS_GREEN_LED, LOW);
+/********************************
+ * Initialises Green and Red LED pins.
+ *********************************/
+void init_LED_pins() {
+  pinMode(GREEN_LED_PIN, OUTPUT);
+  pinMode(RED_LED_PIN, OUTPUT);
+
+  digitalWrite(RED_LED_PIN, LOW);
+  digitalWrite(GREEN_LED_PIN, LOW);
 }
 
+
 /********************************
-*********************************/
+ * Initialises OLED screen.
+ *********************************/
 void init_display() {
    // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
    // SSD1306_EXTERNALVCC
@@ -402,11 +328,14 @@ void init_display() {
 }
 
 /********************************
- * Init TOF sensor
+ * Initialises TOF sensor
  * VL53Lxx-v2
  ********************************/
 void init_vl53lxx_sensor() {
   Serial.println("Init VL53L0X sensor");
+
+  // Shutdown pin
+  pinMode(SENSOR_XSHUT_PIN, OUTPUT);
 
   if (!vl53lxx_sensor.begin()) {
     Serial.println(F("VL53L0X sensor failed!"));
@@ -418,3 +347,169 @@ void init_vl53lxx_sensor() {
   }
 }
 
+/********************************
+ * OFF (shutdown mode)
+ ********************************/
+void vl53lxx_sensor_OFF() {
+  digitalWrite(SENSOR_XSHUT_PIN, HIGH);
+}
+
+/********************************
+ * ON
+ ********************************/
+void vl53lxx_sensor_ON() {
+  digitalWrite(SENSOR_XSHUT_PIN, HIGH);
+}
+
+void init_ecoders() {
+  min_rotary_encoder.begin();
+  min_rotary_encoder.flip();
+
+  max_rotary_encoder.begin();
+  max_rotary_encoder.flip();
+  // Buttons
+  pinMode(MIN_RE_SW_PIN, INPUT_PULLUP);
+  pinMode(MAX_RE_SW_PIN, INPUT_PULLUP);
+
+}
+
+void read_buttons() {
+  static int last_min_btn_state = LOW;
+  static int last_max_btn_state = LOW;
+  // Read
+  int min_btn_state = digitalRead(MIN_RE_SW_PIN);
+  int max_btn_state = digitalRead(MAX_RE_SW_PIN);
+
+  if (min_btn_state != last_min_btn_state) {
+    if (min_btn_state == HIGH) {
+      Serial.println("----MIN BTN");
+      last_min_btn_state = min_btn_state;
+    }
+  }
+  
+  
+  if (max_btn_state != last_max_btn_state) {
+    if (min_btn_state == HIGH) {
+      Serial.println("----MAX BTN");
+      last_max_btn_state = max_btn_state;
+    }
+  }
+  
+
+  delay(10);
+}
+
+/****************************************************
+ * Read Min and Max rotary encoder values
+ ****************************************************/
+void read_encoders() {
+  static int old_max_encoder_count = 0;
+  static int old_min_encoder_count = 0;
+  // Rotary Encoder lugemid
+  // - kui ei keera on 0
+  int new_min_encoder_count = min_rotary_encoder.getCount();
+  int new_max_encoder_count = max_rotary_encoder.getCount();
+
+  // MIN
+  if (new_min_encoder_count > old_min_encoder_count) {
+    selected_value_min++;
+  }
+  else if (new_min_encoder_count < old_min_encoder_count) {
+    selected_value_min--;
+  }
+
+  // MAX
+  if (new_max_encoder_count > old_max_encoder_count) {
+    selected_value_max++;
+  }
+  else if (new_max_encoder_count < old_max_encoder_count) {
+    selected_value_max--;
+  }
+
+  // clamp value
+  if (selected_value_max < ENCODER_MIN_VALUE) {
+    selected_value_max = ENCODER_MIN_VALUE;
+  } else if (selected_value_max > ENCODER_MAX_VALUE) {
+    selected_value_max = ENCODER_MAX_VALUE;
+  }
+
+  old_min_encoder_count = new_min_encoder_count;
+  old_max_encoder_count = new_max_encoder_count;
+}
+
+
+/*******************************************************************
+ * Set staus led:
+ *   @param color - RED or GREEN color if in min/max range.
+ *******************************************************************/
+void set_status_LED(int color) {
+  // If not in min-max range
+  if (distance < selected_value_min || distance > selected_value_max) {
+    if (color == RED) {
+      // Red LED ON
+      digitalWrite(GREEN_LED_PIN, LOW);
+      digitalWrite(RED_LED_PIN, HIGH);
+    }
+    else {
+      // Green LED ON
+      digitalWrite(GREEN_LED_PIN, HIGH);
+      digitalWrite(RED_LED_PIN, LOW);
+    }
+    
+  }
+  else {
+     if (color == RED) {
+      // Green LED ON
+      digitalWrite(GREEN_LED_PIN, HIGH);
+      digitalWrite(RED_LED_PIN, LOW);
+     }
+     else {
+      // Red LED ON
+       digitalWrite(GREEN_LED_PIN, LOW);
+      digitalWrite(RED_LED_PIN, HIGH);
+     }
+  }
+}
+
+/*******************************************************************
+ * @param address  - Memory address to start read
+ *******************************************************************/
+uint32_t read_memory(int address) {
+  uint32_t value = 0;
+  EEPROM.get(address, value);
+  return value;
+}
+
+/*******************************************************************
+ * @param address  - Memory address to start write
+ * @param value - A 32-bit integer to write
+ *******************************************************************/
+int write_memory(int address, uint32_t value) {
+  EEPROM.put(address, value);
+  // Must commit to save to physical Flash!
+  if (EEPROM.commit()) {
+    return 0; // OK
+  } else {
+    return 1; // Error
+  }
+}
+
+uint32_t read_memory_min_value() {
+  uint32_t value = read_memory(ADDRESS_MIN);
+  return value;
+}
+
+uint32_t read_memory_max_value() {
+  uint32_t value = read_memory(ADDRESS_MAX);
+  return value;
+}
+
+int write_memory_min_value(uint32_t value) {
+  int msg = write_memory(ADDRESS_MIN, value);
+  return msg;
+}
+
+int write_memory_max_value(uint32_t value){
+  int msg = write_memory(ADDRESS_MAX, value);
+  return msg;
+}
